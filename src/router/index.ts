b7e5d1d68f905debe2666c69b649022221f3ed45
kalinote <840746219@qq.com>
2023-08-20 11:35:40 +0800

    删除了没用的界面

--- src/router/index.ts
+++ src/router/index.ts
@@ -73,36 +73,6 @@ export const constantRoutes: RouteRecordRaw[] = [
     ]
   },
   {
-    path: "/table",
-    component: Layouts,
-    redirect: "/table/element-plus",
-    name: "Table",
-    meta: {
-      title: "表格",
-      elIcon: "Grid"
-    },
-    children: [
-      {
-        path: "element-plus",
-        component: () => import("@/views/table/element-plus/index.vue"),
-        name: "ElementPlus",
-        meta: {
-          title: "Element Plus",
-          keepAlive: true
-        }
-      },
-      {
-        path: "vxe-table",
-        component: () => import("@/views/table/vxe-table/index.vue"),
-        name: "VxeTable",
-        meta: {
-          title: "Vxe Table",
-          keepAlive: true
-        }
-      }
-    ]
-  },
-  {
     path: "/hook-demo",
     component: Layouts,
     redirect: "/hook-demo/use-fetch-select",
